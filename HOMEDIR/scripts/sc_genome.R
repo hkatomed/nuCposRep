a710eddd0d63fe3f4b4b91b5673d8241117ffcd4
Hiroaki Kato <29747829+hkatomed@users.noreply.github.com>
2022-09-26 12:16:13 +0900

    Add files via upload
    Bug fix

--- HOMEDIR/scripts/sc_genome.R
+++ HOMEDIR/scripts/sc_genome.R
@@ -1,6 +1,6 @@
 setwd("genome")
 library("Biostrings")
-sc.genome <- readDNAStringSet(filepath = "Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz")
+sc.genome <- readDNAStringSet("S288C_reference_sequence_R64-1-1_20110203.fsa")	# bug fixed
 sc.genome <- sc.genome[1:16]
 names(sc.genome) <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", 
 	"chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16")
--- HOMEDIR/scripts/yeast_genomes.R
+++ HOMEDIR/scripts/yeast_genomes.R
@@ -1,6 +1,6 @@
 setwd("genome")
 library(Biostrings)
-sc.genome.2011 <- readDNAStringSet(filepath = "Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa.gz")
+sc.genome.2011 <- readDNAStringSet(filepath = "S288C_reference_sequence_R64-1-1_20110203.fsa") # bug fixed
 sc.genome.2011 <- sc.genome.2011[1:16]
 names(sc.genome.2011) <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", 
 	"chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16")
